8a81eb7452441350899badf1ce00e96baf3bba94
brnstumm <145781192+brnstumm@users.noreply.github.com>
2023-12-12 20:28:22 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+# Import the dependencies.
+from flask import Flask, jsonify
+import sqlalchemy
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///C:/Users/brian.stumm/Desktop/School-Bootcamp/Class_files/sqlalchemy-challenge/SurfsUp/Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+
+# reflect the tables
+Base.classes.keys()
+
+# Save references to each table
+measure = Base.classes.measurement
+station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+
+#################################################
+# Flask Routes
+#################################################
+# Define what to do when a user hits the index route
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return (
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end</br>"
+
+    )
+
